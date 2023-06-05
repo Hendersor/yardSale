ee8bc7dfc4ce64b7bc868817357342b23e861012
Francisco <montejof1998@gmail.com>
2023-06-04 19:22:55 -0700

    main page done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Nav } from "./Nav";
 import { SearchBar } from "./SearchBar";
+import { Products } from "./Products";
 import "../styles/tailwind.css";
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
     <main>
       <Nav />
       <SearchBar />
+      <Products />
     </main>
   );
 };
