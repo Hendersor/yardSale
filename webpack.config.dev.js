7669eea4660165eb8dc21f9b8bdd6510fcfa3a81
Francisco <montejof1998@gmail.com>
2023-06-19 18:57:47 -0700

    some webpack modifications

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,24 +1,17 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
-    publicPath: "/",
   },
-  mode: "production",
   resolve: {
     extensions: [".js", ".jsx"],
-    alias: {
-      "@components": path.resolve(__dirname, "src/components/"),
-      "@styles": path.resolve(__dirname, "src/styles/"),
-    },
   },
-
+  mode: "development",
   module: {
     rules: [
       {
@@ -47,8 +40,12 @@ module.exports = {
       filename: "[name].css",
     }),
   ],
-
-  optimization: {
-    minimizer: [new TerserPlugin()],
+  devServer: {
+    historyApiFallback: true,
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    compress: true,
+    port: 3006,
   },
 };
